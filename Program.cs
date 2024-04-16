f95a1765c92960e1a25c8be949c3fd4d32523be1
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-04-16 09:21:41 +0100

    Call from swagger client whith api key

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using authSwagger;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(/*x=>x.Filters.Add<ApiKeyAuthFilter>()*/);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//Autorise inline config call from swagger whit key
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "Enter key",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Name = "x-api-key",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+
+    });
+    var scheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header,
+    };
+    var requirment = new OpenApiSecurityRequirement
+{
+    {scheme , new List<string>() }
+};
+    c.AddSecurityRequirement(requirment);
+});
 builder.Services.AddScoped<ApiKeyAuthFilter>();
 var app = builder.Build();
 
